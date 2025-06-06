fe86a6a9afca27932f5b35a982600b678231c2e6
Darking01 <enrique.fuentes@unitec.edu>
2025-06-05 23:45:39 -0600

    Prueba
    Commit de prueba

--- app_data/lib/main.dart
+++ app_data/lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutternhsrthrwthrkfyrtyityuytutyrutryutryu6yuthr',
+
       theme: ThemeData(
         // This is the theme of your application.
         //
